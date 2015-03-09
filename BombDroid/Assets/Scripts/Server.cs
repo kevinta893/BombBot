41e544a53140cc055236c59e5ee955c7d3541c99
peterbuk8 <peterbuk8@gmail.com>
2015-03-08 23:46:54 -0600

    Expert mode is now UI.
    No circle yet, just debug info

--- BombDroid/Assets/Scripts/Server.cs
+++ BombDroid/Assets/Scripts/Server.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Server : MonoBehaviour
@@ -10,8 +11,11 @@ public class Server : MonoBehaviour
 	private const bool USE_NAT = false;					//enable nat punchtrough for reachable outside internet
 
 
-
 	private Quaternion cameraRotation;
+
+	public Text infoText;
+	public Text debugText;
+	public Text startServerButtonText;
 
 	// Use this for initialization
 	void Start ()
@@ -22,10 +26,22 @@ public class Server : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (Network.peerType == NetworkPeerType.Disconnected) {
+			infoText.text = "Server disconnected.";
+		} 
+		else if (Network.peerType == NetworkPeerType.Server) 
+		{
+			infoText.text = "Server connected." +
+				"\nConnections: " + Network.connections.Length +
+				"\nMy IP: " + Network.player.ipAddress;
+				
+			debugText.text = "Rotation: " + cameraRotation;	
+		}
+		
+		
 	}
 
-	void OnGUI() 
+	/*void OnGUI() 
 	{
 		if (Network.peerType == NetworkPeerType.Disconnected) 
 		{
@@ -51,8 +67,10 @@ public class Server : MonoBehaviour
 
 
 		}
-	}
+	}*/
 
+
+	// TODO: UNUSED? What do we do with this?
 	bool ConnectToServer (string ip)
 	{
 		if (Network.peerType == NetworkPeerType.Disconnected)
@@ -71,15 +89,29 @@ public class Server : MonoBehaviour
 		return false;
 	}
 
+	public void ToggleServer() 
+	{
+		
+		if (Network.peerType == NetworkPeerType.Disconnected) 
+		{	// start server if not on
+			if (InitializeServer())
+				startServerButtonText.text = "Disconnect";
+		} 
+		else if (Network.peerType == NetworkPeerType.Server) 
+		{	// stop server if on
+			if (StopServer())
+				startServerButtonText.text = "Start Server";
+		}
+
+	}
+
 
 	//attempt to intialize server, returns true if server started, false otherwise
 	public bool InitializeServer ()
 	{
-
 		//make only on no connection
 		if (Network.peerType == NetworkPeerType.Disconnected)
 		{
-
 			NetworkConnectionError err = Network.InitializeServer (MAX_CLIENTS, LISTEN_PORT, false);
 					
 			Debug.Log (Network.player.ipAddress);
@@ -87,20 +119,20 @@ public class Server : MonoBehaviour
 			if (err != NetworkConnectionError.NoError)
 			{
 				Debug.Log (err.ToString ());
+				debugText.text = "Failed to connect.";
 				return false;
 			}
-			return true;
-					
 		}
-
-		return false;
-				
+		return true;
 	}
 
 	public bool StopServer ()
 	{
-		Network.Disconnect (250);
-		return true;
+		if (Network.peerType == NetworkPeerType.Server) {
+			Network.Disconnect (250);
+			return true;
+		}
+		return false;
 	}
 
 
