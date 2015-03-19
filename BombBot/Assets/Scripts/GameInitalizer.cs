e7900f62e711936f9b49b244ee941652d328d18c
kevinta893 <kevinta893@gmail.com>
2015-03-18 22:45:12 -0600

    Basic Main Menu Starts scenes
    Main Menu should be scene 0
    All other scenes should be in the build

---
+++ BombBot/Assets/Scripts/GameInitalizer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameInitalizer : MonoBehaviour {
+
+
+	public GameObject gameMenu;						//group of UI objects join and create
+	public GameObject joinGameMenu;					//group of UI objects for IP and Connect
+	public Text ipText;
+
+	private string ip;
+
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	/* Starts the player off as the expert. Loads the server
+	 * 
+	 */
+	public void StartServer(){
+		Application.LoadLevel("ExpertMode");
+	}
+
+	/* Joins a game
+	 *
+	 */
+	public void JoinGame(){
+		this.ip = ipText.text;
+		Application.LoadLevel("ClientMode");
+	}
+
+
+	/* Shows the GUI for joining a game
+	 * 
+	 */
+	public void ShowJoinGameGUI(){
+		gameMenu.SetActive (false);
+		joinGameMenu.SetActive (true);
+	}
+
+	public void ShowGameMenu(){
+		gameMenu.SetActive (true);
+		joinGameMenu.SetActive (false);
+	}
+
+	public string GetIP(){
+		return ip;
+	}
+}
