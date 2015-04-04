6b8e6d75f069cdce5ccdf9c6b7fc1134f007684a
kevinta893 <kevinta893@gmail.com>
2015-04-03 21:45:39 -0600

    Defuse Panel Resized to Scale

--- BombBot/Assets/Scripts/DefusePanel.cs
+++ BombBot/Assets/Scripts/DefusePanel.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DefusePanel : MonoBehaviour {
 
 
-	private float START_Y;
-
+	private static int PANEL_HEIGHT = 50;
+	public Canvas canvas;
 	// Use this for initialization
 	void Start () {
-		START_Y = this.transform.position.y;
+		RectTransform rectTransform = this.GetComponent<RectTransform> ();
+
+
+
+		rectTransform.offsetMax = new Vector2 (rectTransform.offsetMax.x, -1*(Screen.height - PANEL_HEIGHT) * canvas.scaleFactor);
+
 		//this.transform.position.Set (this.transform.position.x, START_Y - 1000, this.transform.position.z);
 		this.gameObject.SetActive (false);
 	}
@@ -20,13 +26,13 @@ public class DefusePanel : MonoBehaviour {
 
 	public void HideDefusePanel(){
 		Debug.Log ("HIDING");
-		this.transform.position.Set (this.transform.position.x, START_Y - 1000, this.transform.position.z);
+		//this.transform.position.Set (this.transform.position.x, START_Y - 1000, this.transform.position.z);
 		this.gameObject.SetActive (false);
 	}
 
 	public void ShowDefusePanel(){
 		Debug.Log ("SHowing");
-		this.transform.position.Set (this.transform.position.x, START_Y + 1000, this.transform.position.z);
+		//this.transform.position.Set (this.transform.position.x, START_Y + 1000, this.transform.position.z);
 		this.gameObject.SetActive (true);
 	}
 }
